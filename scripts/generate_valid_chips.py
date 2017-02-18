1cdd0fa1a1eadac2466bf9d6853d23025e720184
Your Name <you@example.com>
2017-02-18 00:38:19 -0600

    lump all 330 samples

--- scripts/generate_valid_chips.py
+++ scripts/generate_valid_chips.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import numpy as np
-"""
-proj_dir = '/Users/KANG/geneoscopy_dev/data/20170205_combined_projects_batch1-17/'
-fn_expr = proj_dir + 'chipdata_geneset_x_valid_chips.txt'
+
+## new split 
+proj_dir = '/Users/KANG/geneoscopy_dev/data/20170217_combined_projects_batch1-17/'
+fn_expr = proj_dir + 'chipdata_header.txt'
 fn_vc = proj_dir + 'valid_chips_x.txt'
 
 f = open(fn_expr, 'r')
@@ -10,12 +11,9 @@ header = f.readline().strip().split("\t")
 header = np.array(header[1:])
 f.close()
 
-total_c_tr = 76
-total_p_tr = 62
-total_n_tr = 66
-total_c_te = 19
-total_p_te = 16
-total_n_te = 17
+total_c_te = 23
+total_p_te = 20
+total_n_te = 22
 
 indx_c = []
 indx_p = []
@@ -34,24 +32,23 @@ out = np.hstack((header[np.newaxis].T,
 out[indx_c,1] = '2'
 out[indx_p,1] = '1'
 
-out[np.random.choice(indx_c,size=total_c_te,replace=False),2] = '1'
-out[np.random.choice(indx_p,size=total_p_te,replace=False),2] = '1'
-out[np.random.choice(indx_n,size=total_n_te,replace=False),2] = '1'
+# out[np.random.choice(indx_c,size=total_c_te,replace=False),2] = '1'
+# out[np.random.choice(indx_p,size=total_p_te,replace=False),2] = '1'
+# out[np.random.choice(indx_n,size=total_n_te,replace=False),2] = '1'
 
-# indx_c = np.array(indx_c)
-# indx_p = np.array(indx_p)
-# indx_n = np.array(indx_n)
-# out[indx_c[np.linspace(0,len(indx_c)-1,num=total_c_te,dtype=int)],2] = '1'
-# out[indx_p[np.linspace(0,len(indx_p)-1,num=total_p_te,dtype=int)],2] = '1'
-# out[indx_n[np.linspace(0,len(indx_n)-1,num=total_n_te,dtype=int)],2] = '1'
+indx_c = np.array(indx_c)
+indx_p = np.array(indx_p)
+indx_n = np.array(indx_n)
+out[indx_c[np.linspace(0,len(indx_c)-1,num=total_c_te,dtype=int)],2] = '1'
+out[indx_p[np.linspace(0,len(indx_p)-1,num=total_p_te,dtype=int)],2] = '1'
+out[indx_n[np.linspace(0,len(indx_n)-1,num=total_n_te,dtype=int)],2] = '1'
 
 np.savetxt(fn_vc, out, fmt="%s", delimiter="\t")
+
+
+
 """
-
-
-
-
-
+## add split to existed split
 proj_dir = '/Users/KANG/geneoscopy_dev/data/20170205_combined_projects_batch1-17/'
 fn_in = proj_dir + '../20161115_combined_project_2283_abcdefghi/valid_chips.txt'
 fn_expr = proj_dir + 'chipdata_geneset_x_valid_chips.txt'
@@ -117,3 +114,4 @@ out2[indx_p[np.linspace(0,len(indx_p)-1,num=(total_p_te-p_te_existed),dtype=int)
 out2[indx_n[np.linspace(0,len(indx_n)-1,num=(total_n_te-n_te_existed),dtype=int)],2] = '1'
 
 np.savetxt(fn_vc, np.vstack((out1, out2)), fmt="%s", delimiter="\t")
+"""
--- scripts/run_pipeline_3.sh
+++ scripts/run_pipeline_3.sh
@@ -6,7 +6,7 @@ DIR_SCRIPTS=/Users/KANG/geneoscopy_dev/scripts
 DIR_DATA=/Users/KANG/geneoscopy_dev/data/run_proj_batch1-17_3
 GROUP=N_vs_P_vs_C
 GENE_FILTER=genecards_nanostring_civic
-NORMALIZED_CHIPDATA_FULL=${DIR_DATA}/chipdata_rma.expression_console.good_rin.TXT
+NORMALIZED_CHIPDATA_FULL=${DIR_DATA}/chipdata_rma.expression_console.sst_rma.txt
 # NORMALIZED_CHIPDATA=${DIR_DATA}/chipdata_rma.expression_console.${GENE_FILTER}.txt
 # GENE_FILTER_LST=${DIR_DATA}/../external_data/nanostring/PanCancer_nanostring_genes_annotated.txt
 # GENE_FILTER_LST=${DIR_DATA}/../external_data/Genecards_colon_cancer/GeneCards_Nanostring_genes_annotated.txt  
@@ -27,23 +27,24 @@ echo ""
 
 
 echo "Preparing normalized data ..."
-# python ${DIR_SCRIPTS}/prepare_normalized_expr_data.py -i $NORMALIZED_CHIPDATA_FULL -l $GENE_FILTER_LST -c 2 -o1 ${DIR_DATA}/chipdata_geneset_x_valid_chips.txt -o2 ${DIR_DATA}/chipdata_geneset_x_valid_chips_full.txt
-# ##### Use full list of probesets
-# # cp ${DIR_DATA}/chipdata_geneset_x_valid_chips_full.txt ${DIR_DATA}/chipdata_geneset_x_valid_chips.txt
-# #####
+python ${DIR_SCRIPTS}/prepare_normalized_expr_data.py -i $NORMALIZED_CHIPDATA_FULL -l $GENE_FILTER_LST -c 2 -o1 ${DIR_DATA}/chipdata_geneset_x_valid_chips.txt -o2 ${DIR_DATA}/chipdata_geneset_x_valid_chips_full.txt
+#### Use full list of probesets
+# cp ${DIR_DATA}/chipdata_geneset_x_valid_chips_full.txt ${DIR_DATA}/chipdata_geneset_x_valid_chips.txt
+####
 
 echo "Splitting training/testing sets ... "
-# mkdir -p ${DIR_DATA}/training
-# mkdir -p ${DIR_DATA}/testing
-# python ${DIR_SCRIPTS}/split_expr_train_vs_test.py -v $VALID_CHIPS -i ${DIR_DATA}/chipdata_geneset_x_valid_chips.txt -tr0 ${DIR_DATA}/training/chipdata.txt -tr1 ${DIR_DATA}/training/valid_chips.txt -te0 ${DIR_DATA}/testing/chipdata.txt -te1 ${DIR_DATA}/testing/valid_chips.txt
-# python ${DIR_SCRIPTS}/split_expr_train_vs_test.py -v $VALID_CHIPS -i ${DIR_DATA}/chipdata_geneset_x_valid_chips_full.txt -tr0 ${DIR_DATA}/training/chipdata_full.txt -te0 ${DIR_DATA}/testing/chipdata_full.txt
-
+mkdir -p ${DIR_DATA}/training
+mkdir -p ${DIR_DATA}/testing
+python ${DIR_SCRIPTS}/split_expr_train_vs_test.py -v $VALID_CHIPS -i ${DIR_DATA}/chipdata_geneset_x_valid_chips.txt -tr0 ${DIR_DATA}/training/chipdata.txt -tr1 ${DIR_DATA}/training/valid_chips.txt -te0 ${DIR_DATA}/testing/chipdata.txt -te1 ${DIR_DATA}/testing/valid_chips.txt
+python ${DIR_SCRIPTS}/split_expr_train_vs_test.py -v $VALID_CHIPS -i ${DIR_DATA}/chipdata_geneset_x_valid_chips_full.txt -tr0 ${DIR_DATA}/training/chipdata_full.txt -te0 ${DIR_DATA}/testing/chipdata_full.txt
 
 
 # echo "Analyzing DE genes ... "
-# Rscript ${DIR_SCRIPTS}/de_analysis.r ${DIR_DATA}/training/chipdata.txt ${DIR_DATA}/training/valid_chips.txt $GROUP 1 0 ${DIR_DATA}/training/top_de_genes.txt
+# 	Rscript ${DIR_SCRIPTS}/de_analysis.r ${DIR_DATA}/training/chipdata.txt ${DIR_DATA}/training/valid_chips.txt $GROUP 1 0 ${DIR_DATA}/training/top_de_genes.txt
 
-echo "Analyzing CV DE genes ... "
+# python ${DIR_SCRIPTS}/combine_de_genes.py ${DIR_DATA}/training/top_de_genes.constrained_gene_set.txt ${DIR_DATA}/training/top_de_genes.all_gene_symbol.txt 0.005 0.0005 ${DIR_DATA}/training/top_de_genes.txt
+
+# echo "Analyzing CV DE genes ... "
 # mkdir -p ${DIR_DATA}/training/cv_folds
 # python ${DIR_SCRIPTS}/split_cv_folds.py -i ${DIR_DATA}/training/chipdata.txt -v ${DIR_DATA}/training/valid_chips.txt -f $CV_FOLDS -g $GROUP -o ${DIR_DATA}/training/cv_folds
 # for ((i=1; i<=$CV_FOLDS; i++)); do
@@ -52,50 +53,55 @@ echo "Analyzing CV DE genes ... "
 # done
 
 
-# # echo "Analyzing PvsN, CvsN DE genes ... "
-# # Rscript ${DIR_SCRIPTS}/de_analysis.r ${DIR_DATA}/training/chipdata.txt ${DIR_DATA}/training/valid_chips.p_vs_n.txt N_vs_C 1 0 ${DIR_DATA}/training/top_de_genes.p_vs_n.txt
-# # Rscript ${DIR_SCRIPTS}/de_analysis.r ${DIR_DATA}/training/chipdata.txt ${DIR_DATA}/training/valid_chips.c_vs_n.txt N_vs_C 1 0 ${DIR_DATA}/training/top_de_genes.c_vs_n.txt
+# echo "Analyzing PvsN, CvsN DE genes ... "
+# Rscript ${DIR_SCRIPTS}/de_analysis.r ${DIR_DATA}/training/chipdata.txt ${DIR_DATA}/training/valid_chips.p_vs_n.txt N_vs_C 1 0 ${DIR_DATA}/training/top_de_genes.p_vs_n.txt
+# Rscript ${DIR_SCRIPTS}/de_analysis.r ${DIR_DATA}/training/chipdata.txt ${DIR_DATA}/training/valid_chips.c_vs_n.txt N_vs_C 1 0 ${DIR_DATA}/training/top_de_genes.c_vs_n.txt
 
 
-NUM_TOP_GENES_LIST=( 400 )
+NUM_TOP_GENES_LIST=( 50 100 150 200 400 )
 for NUM_TOP_GENES in "${NUM_TOP_GENES_LIST[@]}"; do
 	echo "#################################"
 	echo "top genes -->" $NUM_TOP_GENES
 	echo "#################################"
 	echo ""
-	python ${DIR_SCRIPTS}/summarize_cv_de_genes.py -i ${DIR_DATA}/training/cv_folds -f $CV_FOLDS -t $NUM_TOP_GENES -o ${DIR_DATA}/training/top_de_genes.txt
+	# python ${DIR_SCRIPTS}/summarize_cv_de_genes.py -i ${DIR_DATA}/training/cv_folds -f $CV_FOLDS -t $NUM_TOP_GENES -o ${DIR_DATA}/training/top_de_genes.txt
+
+	# head -$((NUM_TOP_GENES/2+1)) ${DIR_DATA}/training/top_de_genes.p_vs_n.txt > ${DIR_DATA}/training/top_de_genes.txt
+	# head -$((NUM_TOP_GENES/2+1)) ${DIR_DATA}/training/top_de_genes.c_vs_n.txt | tail -$((NUM_TOP_GENES/2)) >> ${DIR_DATA}/training/top_de_genes.txt
 
 
-	# echo "Analyzing DE genes ... "
-	# Rscript ${DIR_SCRIPTS}/de_analysis.r ${DIR_DATA}/training/chipdata.txt ${DIR_DATA}/training/valid_chips.txt $GROUP 1 0 ${DIR_DATA}/training/top_de_genes.txt
-	# head -$((NUM_TOP_GENES+1)) ${DIR_DATA}/training/top_de_genes.txt > ${DIR_DATA}/training/top_de_genes.txt
+	echo "Analyzing DE genes ... "
+	Rscript ${DIR_SCRIPTS}/de_analysis.r ${DIR_DATA}/training/chipdata.txt ${DIR_DATA}/training/valid_chips.txt $GROUP 1 0 ${DIR_DATA}/training/top_de_genes.txt
+	cp ${DIR_DATA}/training/top_de_genes.txt ${DIR_DATA}/training/tmp.txt
+	head -$((NUM_TOP_GENES+1)) ${DIR_DATA}/training/tmp.txt > ${DIR_DATA}/training/top_de_genes.txt
+	rm ${DIR_DATA}/training/tmp.txt
 
 
 	# ML_MODELS=(random_forest svm neural_net grad_boosting adaboost gauss_process)
-	# # ML_MODELS=(grad_boosting)
-	# for ML_MODEL in "${ML_MODELS[@]}"; do
-	# 	echo "###" $ML_MODEL "###"
+	ML_MODELS=( grad_boosting )
+	for ML_MODEL in "${ML_MODELS[@]}"; do
+		echo "###" $ML_MODEL "###"
 		
-	# 	echo "Preparing training/testing datasets ... "
-	# 	python ${DIR_SCRIPTS}/convert_expr_for_ml.py -t ${DIR_DATA}/training/top_de_genes.txt -i ${DIR_DATA}/training/chipdata.txt -o ${DIR_DATA}/training/training_set.txt
-	# 	python ${DIR_SCRIPTS}/convert_expr_for_ml.py -t ${DIR_DATA}/training/top_de_genes.txt -i ${DIR_DATA}/testing/chipdata.txt -o ${DIR_DATA}/testing/testing_set.txt
-	# 	python ${DIR_SCRIPTS}/incorporate_patient_info.py -p ${PATIENT_SHEET} -d ${DIR_DATA}/training/training_set.txt
-	# 	python ${DIR_SCRIPTS}/incorporate_patient_info.py -p ${PATIENT_SHEET} -d ${DIR_DATA}/testing/testing_set.txt 
+		echo "Preparing training/testing datasets ... "
+		python ${DIR_SCRIPTS}/convert_expr_for_ml.py -t ${DIR_DATA}/training/top_de_genes.txt -i ${DIR_DATA}/training/chipdata.txt -o ${DIR_DATA}/training/training_set.txt
+		python ${DIR_SCRIPTS}/convert_expr_for_ml.py -t ${DIR_DATA}/training/top_de_genes.txt -i ${DIR_DATA}/testing/chipdata.txt -o ${DIR_DATA}/testing/testing_set.txt
+		python ${DIR_SCRIPTS}/incorporate_patient_info.py -p ${PATIENT_SHEET} -d ${DIR_DATA}/training/training_set.txt
+		python ${DIR_SCRIPTS}/incorporate_patient_info.py -p ${PATIENT_SHEET} -d ${DIR_DATA}/testing/testing_set.txt 
 
-	# 	python ${DIR_SCRIPTS}/convert_expr_for_ml.py -i ${DIR_DATA}/training/chipdata_full.txt -o ${DIR_DATA}/training/training_set_full.txt
-	# 	python ${DIR_SCRIPTS}/convert_expr_for_ml.py -i ${DIR_DATA}/testing/chipdata_full.txt -o ${DIR_DATA}/testing/testing_set_full.txt
+		# python ${DIR_SCRIPTS}/convert_expr_for_ml.py -i ${DIR_DATA}/training/chipdata_full.txt -o ${DIR_DATA}/training/training_set_full.txt
+		# python ${DIR_SCRIPTS}/convert_expr_for_ml.py -i ${DIR_DATA}/testing/chipdata_full.txt -o ${DIR_DATA}/testing/testing_set_full.txt
 
-	# 	echo "Training models ... "
-	# 	rm -rf $ML_MODEL
-	# 	python ${DIR_SCRIPTS}/crc_training.py -i ${DIR_DATA}/training/training_set.txt -f ${DIR_DATA}/training/training_set_full.txt -a $ML_MODEL -o ${DIR_DATA}/training/${ML_MODEL} -s ${DIR_DATA}/training/predictor_normal_stats.txt
+		echo "Training models ... "
+		rm -rf $ML_MODEL
+		python ${DIR_SCRIPTS}/crc_training.py -i ${DIR_DATA}/training/training_set.txt -f ${DIR_DATA}/training/training_set_full.txt -a $ML_MODEL -o ${DIR_DATA}/training/${ML_MODEL} -s ${DIR_DATA}/training/predictor_normal_stats.txt -cv True
 
-	# 	echo "Testing prediction ... "
-	# 	python ${DIR_SCRIPTS}/crc_prediction.py -i ${DIR_DATA}/testing/testing_set.txt -f ${DIR_DATA}/testing/testing_set_full.txt -a $ML_MODEL -m ${DIR_DATA}/training/${ML_MODEL}/${ML_MODEL}_model.pkl -s ${DIR_DATA}/training/predictor_normal_stats.txt
-	# 	echo ""
-	# done
+		echo "Testing prediction ... "
+		python ${DIR_SCRIPTS}/crc_prediction.py -i ${DIR_DATA}/testing/testing_set.txt -f ${DIR_DATA}/testing/testing_set_full.txt -a $ML_MODEL -m ${DIR_DATA}/training/${ML_MODEL}/${ML_MODEL}_model.pkl -s ${DIR_DATA}/training/predictor_normal_stats.txt
+		echo ""
+	done
 
-	
-	echo "Preparing training/testing datasets ... "
+
+	# echo "Preparing training/testing datasets ... "
 	# python ${DIR_SCRIPTS}/convert_expr_for_ml.py -t ${DIR_DATA}/training/top_de_genes.txt -i ${DIR_DATA}/training/chipdata.txt -o ${DIR_DATA}/training/training_set.txt
 	# python ${DIR_SCRIPTS}/convert_expr_for_ml.py -t ${DIR_DATA}/training/top_de_genes.txt -i ${DIR_DATA}/testing/chipdata.txt -o ${DIR_DATA}/testing/testing_set.txt
 	# python ${DIR_SCRIPTS}/incorporate_patient_info.py -p ${PATIENT_SHEET} -d ${DIR_DATA}/training/training_set.txt
@@ -104,12 +110,12 @@ for NUM_TOP_GENES in "${NUM_TOP_GENES_LIST[@]}"; do
 	# python ${DIR_SCRIPTS}/convert_expr_for_ml.py -i ${DIR_DATA}/training/chipdata_full.txt -o ${DIR_DATA}/training/training_set_full.txt
 	# python ${DIR_SCRIPTS}/convert_expr_for_ml.py -i ${DIR_DATA}/testing/chipdata_full.txt -o ${DIR_DATA}/testing/testing_set_full.txt
 
-	echo "Training models ... "
+	# echo "Training models ... "
 	# rm -rf ${DIR_DATA}/training/ensemble
 	# python ${DIR_SCRIPTS}/crc_training_ensemble.py -i ${DIR_DATA}/training/training_set.txt -f ${DIR_DATA}/training/training_set_full.txt -o ${DIR_DATA}/training/ensemble -s ${DIR_DATA}/training/predictor_normal_stats.txt
 
-	echo "Testing prediction ... "
-	python ${DIR_SCRIPTS}/crc_prediction_ensemble.py -i ${DIR_DATA}/testing/testing_set.txt -f ${DIR_DATA}/testing/testing_set_full.txt -m ${DIR_DATA}/training/ensemble -s ${DIR_DATA}/training/predictor_normal_stats.txt
-	echo ""
+	# echo "Testing prediction ... "
+	# python ${DIR_SCRIPTS}/crc_prediction_ensemble.py -i ${DIR_DATA}/testing/testing_set.txt -f ${DIR_DATA}/testing/testing_set_full.txt -m ${DIR_DATA}/training/ensemble -s ${DIR_DATA}/training/predictor_normal_stats.txt
+	# echo ""
 
 done
